00f977402d4a1921b07d566b5f5312693573b5ed
Thomas Erhel <thomas.erhel@gmail.com>
2021-07-25 19:36:54 +0200

    First commit ✨

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Introduction to Information Security with HelmetJS Challenges](https://www.freecodecamp.org/learn/information-security/information-security-with-helmetjs/)
+
+[![Run on Repl.it](https://repl.it/badge/github/ThomasErhel/boilerplate-infosec)](https://replit.com/@ThomasErhel/boilerplate-project-infosec)
---
+++ myApp.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var helmet = require('helmet');
+var app = express();
+var ninetyDaysInMilliseconds = 90*24*60*60*1000;
+
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.xssFilter({}));
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({ maxAge: ninetyDaysInMilliseconds, force: true }));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }} ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+var api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
